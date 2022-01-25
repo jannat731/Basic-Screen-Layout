5306b1832c10b35e205963f6e2453511ac42a398
jannat731 <ummasoneyatuljannat33@gmail.com>
2022-01-25 22:39:10 +0600

    My Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'home.dart';
-
 void main(){
   runApp(MyApp());
 }
@@ -10,16 +7,51 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(
-      SystemUiOverlayStyle(statusBarColor: Colors.transparent),
-    );
     return MaterialApp(
-      home: HomePage(),
+      home: MyHome(),
+    );
+  }
+}
+class MyHome extends StatefulWidget {
+  const MyHome({Key? key}) : super(key: key);
+
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  bool _switch=false;
+  ThemeData _dark= ThemeData(brightness: Brightness.dark);
+  ThemeData _light= ThemeData(brightness: Brightness.light);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.cyan,
-        accentColor: Colors.amber,
+      theme: _switch ? _dark: _light,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dark/Light Theme'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.lightbulb,size: 90,color: Colors.amberAccent,),
+              SizedBox(height: 30,),
+              Text('Change Theme',style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold,color: Colors.cyan,letterSpacing:2),
+              ),
+              Switch(value: _switch, onChanged:(_newvalue){
+                setState(() {
+                  _switch= _newvalue;
+                });
+              }
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
